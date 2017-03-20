78acb0eddfacceda52b47ca6742bb4474fa53c74
Christian Todd <christiantodd27@icloud.com>
2017-03-20 16:25:43 -0400

    add event listners and adjust DOM manipulation.

--- app.js
+++ app.js
@@ -40,18 +40,52 @@ function deleteItem(state, itemIndex){
 //DOM MANIPULATION
 
 function renderItem(item, itemId, itemTemplate, itemDataAttr) {
-  var entry = $(listItemTemplate);
-  entry.find('js-shopping-item').text(item.item);
+  var entry = $(itemTemplate);
+  entry.find('.js-shopping-item').text(item.item);
   if (item.purchased) {
-    entry.find('js-shopping-item').addClass('shopping-item__checked');
+    entry.find('.js-shopping-item').addClass('shopping-item__checked');
   }
-  entry.find('js-shopping-item-toggle'); //
+  entry.find('.js-shopping-item-toggle'); //
   entry.attr(itemDataAttr, itemId); //
   return entry;
 }
 
 function renderList(state, listElement, itemDataAttr) {
-  var itemHTML = state.items.map((item, index) =>
-    renderItem(item, index, listElement, itemDataAttr));
-  listElement.html(itemHTML);
+  var itemsHTML = state.items.map((item, index) =>
+    renderItem(item, index, listItemTemplate, itemDataAttr));
+  listElement.html(itemsHTML);
+}
+
+//EVENT LISTENERS
+
+function handleItemAdds(formElement, newItemIdentifier, itemDataAttr, listElement, state) {
+  formElement.submit(function (e) {
+    e.preventDefault();
+    var newItem = formElement.find(newItemIdentifier).val();
+    createItem(state, newItem);
+    renderList(state, listElement, itemDataAttr);
+    this.reset();
+  });
+}
+
+function handleItemDeletes(formElement, removeIdentifier, itemDataAttr, listElement, state) {
+  listElement.on('click', removeIdentifier, function (e) {
+    var itemIndex = parseInt($(this).closest('li').attr(itemDataAttr));
+    deleteItem(state, itemIndex);
+    renderList(state, listElement, itemDataAttr);
+  });
+}
+
+
+function handleItemToggles(listElement, toggleIdentifier, itemDataAttr, state) {
+  listElement.on('click', toggleIdentifier, function (e) {
+    var itemId = $(this.closest('li')).attr(itemDataAttr);
+    var oldItem = retrieveItem(state, itemId);
+
+    updateItem(state, itemId, {
+      item: oldItem.item,
+      purchased: !oldItem.purchased
+    });
+    renderList(state, listElement, itemDataAttr)
+  });
 }
